633cd24968a1bc1ac4cf48c2b34a090455f48d46
DELL <buichithanh24122000@gmail.com>
2021-06-11 11:43:18 +0700

    á

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function home(){
+        return view('home');
+    }
+    public function aboutus(){
+        return view('about-us');
+    }
+}
---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,5 @@
+@extends("layout")
+@section("main")
+    <h2>Đây là nội dụng của about us</h2>
+@endsection
+
