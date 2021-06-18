75f84bf0e60c502382795549a8bd64d5bc15bdd6
DELL <buichithanh24122000@gmail.com>
2021-06-18 11:04:01 +0700

    á

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
 //            CategorySeeder::class,
-            BrandSeeder::class,
-            ProductSeeder::class,
+//            BrandSeeder::class,
+//            ProductSeeder::class,
 
         ]);
     }
